c326b305a1100736e0314b9690156c45f4aeb26e
Atreya Bain <atreyabain@gmail.com>
2024-02-05 22:16:20 +0530

    Hello World

--- src/main/java/org/ieeervce/gatekeeper/GateKeeperApplication.java
+++ src/main/java/org/ieeervce/gatekeeper/GateKeeperApplication.java
@@ -2,9 +2,16 @@ package org.ieeervce.gatekeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GateKeeperApplication {
+	@GetMapping("")
+	String helloWorld(){
+		return "hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GateKeeperApplication.class, args);
