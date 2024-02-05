bb7768558df380ad2197085a00bacfc1cb58eb63
Atreya Bain <atreyabain@gmail.com>
2024-02-05 22:07:47 +0530

    Initialize repository

---
+++ src/main/java/org/ieeervce/gatekeeper/GateKeeperApplication.java
@@ -0,0 +1,13 @@
+package org.ieeervce.gatekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ieeervce/gatekeeper/GateKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ieeervce.gatekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
