6605ebd9d2c7fef3d926e1eca03a50878f855a9d
Chirag <chiragwork2403@gmail.com>
2024-03-03 01:07:17 +0530

    added Request form api

---
+++ src/main/java/org/ieeervce/gatekeeper/service/RequestFormService.java
@@ -0,0 +1,47 @@
+package org.ieeervce.gatekeeper.service;
+
+import jakarta.transaction.Transactional;
+import org.ieeervce.gatekeeper.ItemNotFoundException;
+import org.ieeervce.gatekeeper.entity.RequestForm;
+import org.ieeervce.gatekeeper.repository.RequestFormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RequestFormService {
+
+    static final String ITEM_NOT_FOUND="Request Form Not Found";
+    private final RequestFormRepository requestFormRepository;
+@Autowired
+RequestFormService(RequestFormRepository requestFormRepository){
+    this.requestFormRepository= requestFormRepository;
+}
+public RequestForm findOne(Long requestFormId)throws ItemNotFoundException{
+    return requestFormRepository.findById(requestFormId).orElseThrow(()-> new ItemNotFoundException(ITEM_NOT_FOUND + requestFormId));
+}
+public RequestForm add(RequestForm requestForm){
+    return requestFormRepository.save(requestForm);
+}
+ public List<RequestForm> list(){
+    return requestFormRepository.findAll();
+ }
+
+ public RequestForm save(RequestForm requestForm){
+    return requestFormRepository.save(requestForm);
+ }
+ public void delete(Long requestFormId){
+    requestFormRepository.deleteById(requestFormId);
+ }
+ @Transactional
+ public RequestForm edit(Long requestFormId, RequestForm editedRequestForm) throws ItemNotFoundException{
+     Optional<RequestForm> existingRequestForm = requestFormRepository.findById(requestFormId);
+     return existingRequestForm.map((requestForm)->{
+        editedRequestForm.setId(requestFormId);
+        editedRequestForm.setCreatedAt(requestForm.getCreatedAt());
+        return requestFormRepository.save(editedRequestForm);
+     }).orElseThrow(()-> new ItemNotFoundException(ITEM_NOT_FOUND+requestFormId));
+ }
+}
