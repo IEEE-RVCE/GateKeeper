0addf95c2291936004734ad4ef0a3dd5fa6574b4
SundarakrishnanN <sundar050804@gmail.com>
2024-05-25 14:45:32 +0530

    fixed cors bug

--- src/main/java/org/ieeervce/gatekeeper/config/SecurityConfiguration.java
+++ src/main/java/org/ieeervce/gatekeeper/config/SecurityConfiguration.java
@@ -18,6 +18,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -34,24 +39,35 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).authorizeHttpRequests(SecurityConfiguration::getCustomizedHttpAuthorization).csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable);
+
+                .cors(customizer->customizer.configurationSource(corsConfigurationSource()));
         return http.build();
     }
     private static void getCustomizedHttpAuthorization(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry customizer) {
         customizer
 
-               .requestMatchers(HttpMethod.POST,"/user").hasRole("Admin")
-               .requestMatchers(HttpMethod.PUT,"/user").hasRole("Admin")
-               .requestMatchers(HttpMethod.DELETE,"/user").hasRole("Admin")
-               .requestMatchers("/role").hasRole("Admin")
-               .requestMatchers("/society").hasRole("Admin")
-               .anyRequest().authenticated();
+//               .requestMatchers(HttpMethod.POST,"/user").hasRole("Admin")
+//               .requestMatchers(HttpMethod.PUT,"/user").hasRole("Admin")
+//               .requestMatchers(HttpMethod.DELETE,"/user").hasRole("Admin")
+//               .requestMatchers("/role").hasRole("Admin")
+//               .requestMatchers("/society").hasRole("Admin")
+               .anyRequest().permitAll();
 
 //                .requestMatchers("/user").hasRole("ADMIN")
 //                .requestMatchers("/role").hasRole("ADMIN")
 //                .requestMatchers("/society").hasRole("ADMIN")
 
 
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://gate.ieee-rvce.org"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
