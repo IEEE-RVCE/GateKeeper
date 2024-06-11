0cdbd5d5b0317ea0bfefae359528e8391bc75129
SundarakrishnanN <sundar050804@gmail.com>
2024-06-11 19:34:06 +0530

    added async

--- src/main/java/org/ieeervce/gatekeeper/GateKeeperApplication.java
+++ src/main/java/org/ieeervce/gatekeeper/GateKeeperApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
+@EnableAsync
 public class GateKeeperApplication {
 	@GetMapping("/")
 	String index() throws Exception{
--- src/main/java/org/ieeervce/gatekeeper/service/EmailService.java
+++ src/main/java/org/ieeervce/gatekeeper/service/EmailService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -20,7 +21,7 @@ public class EmailService {
     @Autowired
     private TemplateEngine templateEngine;
     @Value("${spring.mail.username}") private String sender;
-
+    @Async
     public void sendSimpleMail(EmailDTO emailDetails) throws MessagingException{
 
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
